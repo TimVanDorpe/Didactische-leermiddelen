d673392fd4df96b6a307c9a73f21f1c767b92a37
Thomas Vanderschaeve <thomas.vanderschaeve.v5230@student.hogent.be>
2016-04-07 16:42:49 +0200

    gui uc1 en uc2

--- Groep9Java/src/gui/ProductToevoegenController.java
+++ Groep9Java/src/gui/ProductToevoegenController.java
@@ -7,7 +7,14 @@ package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -15,6 +22,37 @@ import javafx.fxml.Initializable;
  * @author Thomas
  */
 public class ProductToevoegenController implements Initializable {
+
+    @FXML
+    private AnchorPane AnchorPane;
+    @FXML
+    private TextField txfNaam;
+    @FXML
+    private TextArea txfOmschrijving;
+    @FXML
+    private TextField txfArtikelnummer;
+    @FXML
+    private TextField txfPrijs;
+    @FXML
+    private TextField txfAantal;
+    @FXML
+    private TextField txfFirma;
+    @FXML
+    private TextField txfEmailFirma;
+    @FXML
+    private TextField txfDoelgroepen;
+    @FXML
+    private TextField txfLeergebieden;
+    @FXML
+    private CheckBox uitleenbaarheid;
+    @FXML
+    private ImageView imgviewFoto;
+    @FXML
+    private Button btnToevoegen;
+    @FXML
+    private Button btnFoto;
+    @FXML
+    private Button btnAnnuleer;
 
     /**
      * Initializes the controller class.
