57ed5b77a375c7b94d3b45f23155403df6eb483d
Thomas Vanderschaeve <thomas.vanderschaeve.v5230@student.hogent.be>
2016-04-11 08:53:00 +0200

    merge

--- Groep9Java/src/StartUp.java
+++ Groep9Java/src/StartUp.java
@@ -4,38 +4,29 @@ import gui.ProductenFrameController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-/**
- *
- * @author Tim
- */
-public class StartUp extends Application{
-    
-     public static void main(String... args) {
-        Application.launch(StartUp.class, args);
-    }
+public class StartUp extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
-        ProductenBeheer producten = new ProductenBeheer();
-        Scene scene = new Scene(new ProductenFrameController(producten));
-  
+    public void start(Stage stage) {
+        Scene scene = new Scene(new ProductenFrameController(new ProductenBeheer()));
         stage.setScene(scene);
         stage.setTitle("Didactische leermiddelen");
-
         // The stage will not get smaller than its preferred (initial) size.
-        stage.setOnShown(e -> {
+        stage.setOnShown((WindowEvent t) -> {
             stage.setMinWidth(stage.getWidth());
             stage.setMinHeight(stage.getHeight());
         });
-
         stage.show();
     }
-    
+
+    public static void main(String... args) {
+        Application.launch(StartUp.class, args);
+    }
 }
