cdf34f44fc6630b4f8ab5d7ba328791ded9bc548
Jens De Dene <jens.dedene.t9216@student.hogent.be>
2016-04-11 15:31:00 +0200

    implementatie knop Producttoevoegen p1

--- Groep9Java/src/gui/OverzichtProductenController.java
+++ Groep9Java/src/gui/OverzichtProductenController.java
@@ -8,11 +8,18 @@ package gui;
 import domein.Product;
 import domein.ProductController;
 import java.io.IOException;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * FXML Controller class
@@ -33,6 +40,8 @@ public class OverzichtProductenController  extends BorderPane{
     private TableColumn<Product, String> clmPlaats;
     
      private ProductController domeinController;
+    @FXML
+    private Button btnToevoegen;
 
     public OverzichtProductenController(ProductController domeinController) {
         this.domeinController = domeinController;
@@ -66,6 +75,59 @@ public class OverzichtProductenController  extends BorderPane{
         tblProducten.setItems(domeinController.getProductSortedList());
     }
 
+    @FXML
+    private void naarProductPagina(ActionEvent event) {
+        
+        Stage stage = new Stage();
+        stage.setTitle("Product toevoegen");
+
+        Scene scene = new Scene(new ProductToevoegenController(domeinController));
+        stage.setScene(scene);
+
+        this.setDisable(true);
+
+        
+        
+        //Het hoofdvenster mag niet afgesloten worden
+        /*
+         In that case we can use the consume() function of the WindowEvent. 
+         This will consume the event and prevent the window from closing.
+         ------------------------------------------------------------------ */
+        Stage stageProductScherm = (Stage) tblProducten.getScene().getWindow();
+        
+        
+        /*
+        
+        EventHandler handler = event -> event.consume();
+        stageProductScherm.addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, handler);
+
+        //het subvenster mag niet gesloten  worden
+        //----------------------------------------        
+        stage.setOnCloseRequest(handler);
+
+        //luisteraar indien het subscherm gesloten wordt. 
+        //---------------------------------------------
+        stage.addEventHandler(WindowEvent.WINDOW_HIDING,
+                event -> {
+                    OverzichtProductenController.this.setDisable(false);
+                    tblProducten.setItems((ObservableList<Product>) domeinController.geefOverzichtProducten());
+                    stageProductScherm.removeEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, handler);
+                });
+
+        // Het subscherm wordt niet kleiner dan het minimum scherm.
+        //---------------------------------------------------------
+
+        */
+        
+        stage.setOnShown(e -> {
+            stage.setMinWidth(stage.getWidth());
+            stage.setMinHeight(stage.getHeight());
+        });
+
+        stage.show();
+
+        
+    }
     
-   
+    
 }
