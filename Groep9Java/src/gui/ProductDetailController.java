e70d9e05dfd2f009b2cee865fab066879db25f58
Thomas Vanderschaeve <thomas.vanderschaeve.v5230@student.hogent.be>
2016-04-11 15:11:37 +0200

    Foto toevoegen en weergeven bij details en productToevoegen

--- Groep9Java/src/gui/ProductDetailController.java
+++ Groep9Java/src/gui/ProductDetailController.java
@@ -10,11 +10,16 @@ import domein.Firma;
 import domein.Leergebied;
 import domein.Product;
 import domein.ProductController;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -22,12 +27,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
 
-
-public class ProductDetailController extends Pane  implements Observer{
+public class ProductDetailController extends Pane implements Observer {
 
     @FXML
     private AnchorPane AnchorPane;
@@ -42,7 +49,7 @@ public class ProductDetailController extends Pane  implements Observer{
     @FXML
     private TextField txtAantal;
     @FXML
-    private TextField txtFirma , txtPlaats;
+    private TextField txtFirma, txtPlaats;
     @FXML
     private TextField txtEmailFirma;
     @FXML
@@ -61,15 +68,16 @@ public class ProductDetailController extends Pane  implements Observer{
     private Button btnAnnuleer;
 
     private ProductController dc;
-    
+
     /**
      * Initializes the controller class.
      */
-    
-    public ProductDetailController(){
+    final FileChooser fileChooser = new FileChooser();
+
+    public ProductDetailController() {
         // TODO
-       
-     FXMLLoader loader = new FXMLLoader(getClass().getResource("ProductDetail.fxml"));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ProductDetail.fxml"));
         loader.setRoot(this);
         loader.setController(this);
         try {
@@ -77,67 +85,78 @@ public class ProductDetailController extends Pane  implements Observer{
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
-    
-    } 
-    
-    
-     @FXML
+
+    }
+
+    @FXML
     private void wijzigProduct(ActionEvent event) {
-        
+
         String naam = txtNaam.getText();
         String omschrijving = txtOmschrijving.getText();
         int artikkelnummer = Integer.parseInt(txtArtikelnummer.getText());
         double prijs = Double.parseDouble(txtPrijs.getText());
         int aantal = Integer.parseInt(txtAantal.getText());
         String plaats = txtPlaats.getText();
-        
-        
+
         String firmaNaam = txtFirma.getText();
-        if(firmaNaam.isEmpty() ){
+        if (firmaNaam.isEmpty()) {
             firmaNaam = "/";
         }
         String firmaEmail = txtEmailFirma.getText();
-        if(firmaEmail.isEmpty() ||  txtEmailFirma == null){
+        if (firmaEmail.isEmpty() || txtEmailFirma == null) {
             firmaEmail = "/";
         }
-        
-        Firma firma = new Firma(firmaNaam , firmaEmail);
-        
+
+        Firma firma = new Firma(firmaNaam, firmaEmail);
+
         String naamDoelgroep = txtDoelgroepen.getText();
-        if(naamDoelgroep.isEmpty()){
+        if (naamDoelgroep.isEmpty()) {
             naamDoelgroep = "/";
         }
         Doelgroep doelgroep = new Doelgroep(naamDoelgroep);
-        
+
         String namenLeergebieden = txtLeergebieden.getText();
         List<Leergebied> leergebieden = new ArrayList<>();
-        if(namenLeergebieden.isEmpty()){
+        if (namenLeergebieden.isEmpty()) {
             Leergebied leergebied = new Leergebied("/");
             leergebieden.add(leergebied);
-        }else{
-            Leergebied leergebied = new Leergebied(txtLeergebieden.getText());  
+        } else {
+            Leergebied leergebied = new Leergebied(txtLeergebieden.getText());
             leergebieden.add(leergebied);
         }
-        
-        
-        
+
         dc.wijzigProduct(naam, naam, omschrijving, artikkelnummer, prijs, aantal, plaats, firma, doelgroep, leergebieden);
-    }  
-    
-    
-    
+    }
+
+    @FXML
+    private void fotoToevoegen(ActionEvent event) {
+        File file = fileChooser.showOpenDialog(null);
+        FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+         FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+        fileChooser.getExtensionFilters().addAll(extFilterJPG,extFilterPNG);
+
+        try {
+            BufferedImage bufferedImage = ImageIO.read(file);
+            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+            imgViewFoto.setImage(image);
+        } catch (IOException ex) {
+            Logger.getLogger(ProductDetailController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
     //steekt alle gegevens in de textfields
     @Override
     public void update(Observable o, Object arg) {
-       Product product = (Product) arg;
-       txtAantal.setText(Integer.toString(product.getAantal()));
-       txtArtikelnummer.setText(Integer.toString(product.getArtikelnummer()));
-       txtFirma.setText(product.getFirma().getNaam());
-       txtEmailFirma.setText(product.getFirma().getEmailContactPersoon());
-       txtPrijs.setText(Double.toString(product.getPrijs()));
-       txtNaam.setText(product.getNaam());
-       txtOmschrijving.setText(product.getOmschrijving());
-       txtPlaats.setText(product.getPlaats());
-       
+        Product product = (Product) arg;
+        txtAantal.setText(Integer.toString(product.getAantal()));
+        txtArtikelnummer.setText(Integer.toString(product.getArtikelnummer()));
+        txtFirma.setText(product.getFirma().getNaam());
+        txtEmailFirma.setText(product.getFirma().getEmailContactPersoon());
+        txtPrijs.setText(Double.toString(product.getPrijs()));
+        txtNaam.setText(product.getNaam());
+        txtOmschrijving.setText(product.getOmschrijving());
+        txtPlaats.setText(product.getPlaats());
+
     }
 }
