c6b85855192d276cabdbe9fcf2e76f4dfbe0b4f2
Thomas Vanderschaeve <thomas.vanderschaeve.v5230@student.hogent.be>
2016-04-19 15:19:32 +0200

    Login aanmelden geimplementeerd

--- Groep9Java/src/gui/LoginSchermController.java
+++ Groep9Java/src/gui/LoginSchermController.java
@@ -7,13 +7,21 @@ package gui;
 
 import domein.Beheerder;
 import domein.BeheerderController;
+import domein.ProductController;
+import java.io.File;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -39,6 +47,10 @@ public class LoginSchermController extends Pane {
      
 
     private BeheerderController beheerderController;
+    @FXML
+    private Label lblError;
+    @FXML
+    private ImageView imgViewLogo;
 
     public LoginSchermController(BeheerderController beheerderController) {
         this.beheerderController = beheerderController;
@@ -51,19 +63,46 @@ public class LoginSchermController extends Pane {
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
+//          File file = new File("");
+//        Image image = new Image(file.toURI().toString());
+//        imgViewLogo.setImage(image);
     }
     
     
     @FXML
     private void meldAan(ActionEvent event) {
-        beheerderController.meldAan(new Beheerder(txfGerbuikersNaam.getText(), txfWachtwoord.getText()));
         
+        try{
+           
+            if(txfGerbuikersNaam.getText() == null || txfWachtwoord.getText() == null){
+                throw new IllegalArgumentException("Alle velden moeten ingevuld zijn.");
+            }
+             beheerderController.meldAan(new Beheerder(txfGerbuikersNaam.getText(), txfWachtwoord.getText()));
+         Stage st = (Stage) btnAanmelden.getScene().getWindow();
+            st.hide();
+             Stage stage = new Stage();
+        stage.setTitle("Didactische leermiddelen");
+
+        Scene scene = new Scene(new ProductenFrameController(new ProductController()));
+        stage.setScene(scene);
+
+        //this.setDisable(true);
+        stage.show();
+        
+        } catch (IllegalArgumentException ex) {
+
+            lblError.setText(ex.getMessage());
+            lblError.setTextFill(Color.web("#F20000"));
+
+        }
+       
     }
 
     @FXML
     private void registreer(ActionEvent event) {
     }
 
+    //wachtwoord vergeten
     @FXML
     private void wachtWoordVersturenNaarGebruiker(ActionEvent event) {
     }
