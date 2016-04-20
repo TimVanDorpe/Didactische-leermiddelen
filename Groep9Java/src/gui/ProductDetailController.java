d1e94289ba44a52b0fd7ae65b05c7706924cebae
Jens De Dene <jens.dedene.t9216@student.hogent.be>
2016-04-20 19:53:52 +0200

    reservatieknop verwijderd, velden voor toevoegen bij start terug enabled

--- Groep9Java/src/gui/ProductDetailController.java
+++ Groep9Java/src/gui/ProductDetailController.java
@@ -131,26 +131,26 @@ public class ProductDetailController extends Pane implements Observer {
         }
 
         if (dc.getSelectionModelEmpty()) {
-            btnToevoegen.setDisable(true);
-            btnAnnuleer.setDisable(true);
-            btnFoto.setDisable(true);
-            uitleenbaarheid.setDisable(true);
-            txtAantal.setDisable(true);
-            txtArtikelnummer.setDisable(true);
-            txtDoelgroepen.setDisable(true);
-            txtEmailFirma.setDisable(true);
-            txtFirma.setDisable(true);
+            //btnToevoegen.setDisable(true);
+            //btnAnnuleer.setDisable(true);
+            //btnFoto.setDisable(true);
+            //uitleenbaarheid.setDisable(true);
+            //txtAantal.setDisable(true);
+            //txtArtikelnummer.setDisable(true);
+            //txtDoelgroepen.setDisable(true);
+            //txtEmailFirma.setDisable(true);
+            //txtFirma.setDisable(true);
             //txtLeergebieden.setDisable(true);
-            txtNaam.setDisable(true);
-            txtOmschrijving.setDisable(true);
-            txtPlaats.setDisable(true);
-            txtPrijs.setDisable(true);
+            //txtNaam.setDisable(true);
+            //txtOmschrijving.setDisable(true);
+            //txtPlaats.setDisable(true);
+            //txtPrijs.setDisable(true);
             btnWijzigen.setDisable(true);
             btnLeegmaken.setDisable(true);
             btnVerwijderen.setDisable(true);
 
-            btnSelecteerLeergebied.setDisable(true);
-            listLeergebieden.setDisable(true);
+            //btnSelecteerLeergebied.setDisable(true);
+            //listLeergebieden.setDisable(true);
         }
     }
 
